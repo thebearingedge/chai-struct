c1d122e34f986429ecd7cf0953aab9abbe010130
thebearingedge <contact@timdav.is>
2018-02-21 15:14:09 -0800

    Add TypeScript typings for "type-diff" helpers.

--- index.d.ts
+++ index.d.ts
@@ -3,3 +3,9 @@ declare module Chai {
     structure(struct: object|Function): void;
   }
 }
+
+declare module 'chai-struct' {
+  export const Nullable: (value: any) => object
+  export const Optional: (value: any) => object
+  export const Any: (value?: any) => object
+}
