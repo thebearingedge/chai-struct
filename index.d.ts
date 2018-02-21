93841da77de3b8c0d81203d76da39be665b42770
thebearingedge <contact@timdav.is>
2018-02-21 15:18:48 -0800

    Add chaiStruct member to type definitions.

--- index.d.ts
+++ index.d.ts
@@ -5,6 +5,7 @@ declare module Chai {
 }
 
 declare module 'chai-struct' {
+  export const chaiStruct: (chai: any, utils: any) => void
   export const Nullable: (value: any) => object
   export const Optional: (value: any) => object
   export const Any: (value?: any) => object
